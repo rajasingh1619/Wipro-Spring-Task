9a92906a9cd26fbe7018c1bb0f813531da4f35d2
Raja Kumar Singh <91696561+rajasingh1619@users.noreply.github.com>
2024-07-15 10:32:35 +0530

    Add files via upload

---
+++ spring-reactive-client/src/main/java/com/wipro/client/GreetingClient.java
@@ -0,0 +1,28 @@
+package com.wipro.client;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.wipro.model.Greeting;
+
+import reactor.core.publisher.Mono;
+/*
+*
+* The WebClient class uses reactive features, in the form of a Mono to hold the
+* content of the message (returned by the getMessage method).
+*/
+@Component
+public class GreetingClient {
+ private final WebClient client;
+ // Spring Boot auto-configures a `WebClient.Builder` instance with nice defaults and customizations.
+ // We can use it to create a dedicated `WebClient` for our component.
+ public GreetingClient(WebClient.Builder builder) {
+   this.client = builder.baseUrl("http://localhost:9095").build();
+ }
+ public Mono<String> getMessage() {
+   return this.client.get().uri("/hello").accept(MediaType.APPLICATION_JSON)
+       .retrieve()
+       .bodyToMono(Greeting.class)
+       .map(Greeting::getMessage);
+ }
+}
---
+++ spring-rest-template/src/main/java/com/wipro/model/Role.java
@@ -0,0 +1,9 @@
+
+package com.wipro.model;
+
+public enum Role {
+	
+	ADMIN,DOCTOR,GROUP
+
+
+}
